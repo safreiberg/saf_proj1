a06c706fb409a38dc21b7c784ee882fc6ce1546c
Stephen Freiberg <safreiberg@gmail.com>
2012-09-16 15:28:32 -0700

    fixed a 'new site' bug

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -6,6 +6,8 @@ class SitesController < ApplicationController
   end
 
   def list_site
+    @name = params[:id]
+    @site = Site.where(:name => @name).first_or_create({ :name => @name, :hits => 0, :total_duration => 0 })
   end
 
   ## Note that we create the record for a site if the site has never been visited.
